2df4d421ecbebf5e4c3af05a8b83263cfb8d40c8
Makiah Bennett <makiahtbennett@gmail.com>
2018-01-08 14:12:42 -0500

    Start to create initial layout

--- app/src/main/java/com/makiah/cvtuner/MainActivity.java
+++ app/src/main/java/com/makiah/cvtuner/MainActivity.java
@@ -2,17 +2,12 @@ package com.makiah.cvtuner;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class MainActivity extends Activity {
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-opencv");
-    }
-
+public class MainActivity extends Activity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
