2fe1cceb1c799483d4ed2dd0127cd860f489281c
Makiah Bennett <makiahtbennett@gmail.com>
2018-01-25 13:40:15 -0500

    Improve app layout

--- app/src/main/java/com/makiah/cvtuner/MainActivity.java
+++ app/src/main/java/com/makiah/cvtuner/MainActivity.java
@@ -2,13 +2,112 @@ package com.makiah.cvtuner;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
 
-public class MainActivity extends Activity
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener
 {
+    private static final String TAG = "OpenCVCam";
+
+    private CameraBridgeViewBase cameraBridgeViewBase;
+
+    /**
+     * The callback for when OpenCV has finished initialization.
+     */
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    cameraBridgeViewBase.enableView();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set screen on.
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        // Set camera view base properties and direct frames to the restlessness detector.
+        cameraBridgeViewBase = (CameraBridgeViewBase) findViewById(R.id.cameraFeed);
+        cameraBridgeViewBase.enableFpsMeter();
+        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
+    }
+
+    /**
+     * When the user navigates away from the activity.
+     */
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+
+        if (cameraBridgeViewBase != null)
+            cameraBridgeViewBase.disableView();
+    }
+
+    /**
+     * When the user returns to this activity.
+     */
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    /**
+     * When the user ends the activity.
+     */
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+
+        if (cameraBridgeViewBase != null)
+            cameraBridgeViewBase.disableView();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height)
+    {
+    }
+
+    @Override
+    public void onCameraViewStopped()
+    {
+    }
+
+    @Override
+    public Mat onCameraFrame(Mat inputFrame)
+    {
+        return inputFrame;
     }
 }
